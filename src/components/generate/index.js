51ab372470e024fc829bc5bebf5c00edbea98a76
Annie <annie.sun@enzuzo.com>
2021-09-19 01:49:11 -0400

    added graphic & more styling

--- src/components/generate/index.js
+++ src/components/generate/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from './style.module.css'
 
 export default function GenerateButton() {
   return (
-    <button className="featureButton">
-      Generate Mind Map
+    <button className={true ? styles.button : 'featureButton'}>
+      <span style={{ zIndex: 9 }}>Generate Mind Map</span>
+      <div className={styles.hoverEffect}/>
     </button>
   )
 }
--- src/components/pages/Landing/index.js
+++ src/components/pages/Landing/index.js
@@ -9,14 +9,17 @@ export default function Landing() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Fade bottom>
-          <h1>Turn your notes into mindmaps</h1>
-        </Fade>
-        <Fade bottom delay={500}>
-          <Link to='/choose'>
-            <button className={`featureButton`}>Start</button>
-          </Link>
-        </Fade>
+        <div style={{ padding: '42px', boxSize: 'border-box' }}>
+          <Fade bottom>
+            <h1>Turn your notes into a mind map</h1>
+          </Fade>
+          <Fade bottom delay={500}>
+            <Link to='/choose'>
+              <button className={`featureButton`}>Start</button>
+            </Link>
+          </Fade>
+        </div>
+        <img className={styles.image} src='/resources/landing_graphic.png' />
       </div>
     </div >
   )
